9c0722ef3d5016c0546642a1e1500e88b42e9b54
ches <ches@itu.dk>
2021-09-24 12:42:34 +0200

    implemented flatten with lambda

--- Assignment3.Tests/ExtensionsTests.cs
+++ Assignment3.Tests/ExtensionsTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace BDSA2020.Assignment02.Tests
@@ -6,9 +7,17 @@ namespace BDSA2020.Assignment02.Tests
     public class ExtensionsTests
     {
         [Fact]
-        public void Test1()
+        public void flatten_given_List_123_456_789_return_123456789()
         {
+            var expected = new List<int> {1,2,3,4,5,6,7,8,9};
+            var list1 = new List<int>() {1,2,3};
+            var list2 = new List<int>() {4,5,6};
+            var list3 = new List<int>() {7,8,9};
+            var list = new List<int>[] {list1, list2, list3};
 
+            var actual = list.Flatten();
+
+            Assert.Equal(expected, actual);   
         }
     }
 }
--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BDSA2020.Assignment02
 {
     public static class Extensions
     {
+        public static IEnumerable<int> Flatten(this IEnumerable<int>[] xs) => xs.SelectMany(i => i);
     }
 }
