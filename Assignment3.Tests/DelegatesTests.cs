4cb352359b1c7a126fb755f7bc26a7d87a32f0b4
ches <ches@itu.dk>
2021-09-25 18:17:29 +0200

    Implemented ReverseString test as anonymous method

--- Assignment3.Tests/DelegatesTests.cs
+++ Assignment3.Tests/DelegatesTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Xunit;
 
 namespace BDSA2020.Assignment02.Tests
@@ -6,9 +7,16 @@ namespace BDSA2020.Assignment02.Tests
     public class DelegatesTests
     {
         [Fact]
-        public void Test1()
+        public void ReverseString_takes_abcdef_returns_fedcba()
         {
-
+            //Arrange
+            var expected = "fedcba";
+            var input = "abcdef";
+            //Act
+            Func<string, string> reverse = (s) => new string(s.Reverse().ToArray());
+            var actual = reverse(input);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
