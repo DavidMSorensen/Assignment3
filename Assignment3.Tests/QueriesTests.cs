1653acdc20ed9c5c42adae44d2f94a10ebabee78
Stender98 <90451897+Stender98@users.noreply.github.com>
2021-09-27 11:43:20 +0200

    Monday dev (#5)
    * QueriesTest implemented for queries 1-3
    
    * Queries
    
    * Adding wizards and furthering tests
    
    * ListWizardsInReverseOrder tests impemented
    
    * More queries
    
    * Quick test and method fix (with linq)
    
    * YearOfTheFirstSithLordLinq fix (now OrderBy works)
    
    * HarryPotterWizardsLinq implemented
    
    * listWizard query
    
    * Queries done
    
    Co-authored-by: daso <daso@itu.dk>
    Co-authored-by: ches <ches@itu.dk>

--- Assignment3.Tests/QueriesTests.cs
+++ Assignment3.Tests/QueriesTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace BDSA2020.Assignment02.Tests
@@ -6,9 +7,74 @@ namespace BDSA2020.Assignment02.Tests
     public class QueriesTests
     {
         [Fact]
-        public void Test1()
+        public void GetRowling_returns_names_of_wizards_made_by_Rowling()
         {
+            //Arrange
+            string[] expected = {"Harry Potter", "Hermione Granger", "Ronald Weasley", "Voldemort"};
 
+            //Act
+            var actual = Queries.GetWizardsByRowling();
+            var actualLinq = Queries.GetWizardsByRowlingLinq();
+
+            //Assert
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected, actualLinq);
+        }
+
+        [Fact]
+        public void YearOfTheFirstSithLord_returns_1977()
+        {
+            //Arrange
+            var expected = 1977;
+
+            //Act
+            var actual = Queries.YearOfTheFirstSithLord();
+            var actualLinq = Queries.YearOfTheFirstSithLordLinq();
+
+            //Assert
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected, actualLinq);
+        }
+
+        [Fact]
+        public void HarryPotterWizards_returns_tuples_of_name_and_year_of_wizards_from_Harrpy_Potter_books()
+        {
+            //Arrange
+            var expected = new List<(string, int?)>();
+            expected.Add(("Harry Potter", 1997));
+            expected.Add(("Hermione Granger", 2003));
+            expected.Add(("Ronald Weasley", 1997));
+            expected.Add(("Voldemort", 1999));
+
+            //Act
+            var actual = Queries.HarryPotterWizards();
+            var actualLinq = Queries.HarryPotterWizardsLinq();
+
+            //Assert
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected, actualLinq);
+        }
+
+        [Fact]
+        public void ListWizardsInReverseOrder_returns_wizards_in_reverse_order()
+        {
+            //Arrange
+             string[] expected = {
+                "Mickey Mouse", 
+                "Neo", 
+                "Stephen Strange", 
+                "The Wizard of Oz",
+                "Sauron","Gandalf",
+                "Voldemort", "Ronald Weasley", "Hermione Granger", "Harry Potter",
+                "Yoda", "Darth Vader", "Darth Plagueis The Wise", "Darth Maul"};
+
+            //Act
+            var actual = Queries.ListWizardsInReverseOrder();
+            var actualLinq = Queries.ListWizardsInReverseOrderLinq();
+
+            //Assert
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected, actualLinq);
         }
     }
 }
--- Assignment3/Queries.cs
+++ Assignment3/Queries.cs
@@ -1,6 +1,91 @@
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
 namespace BDSA2020.Assignment02
 {
     public class Queries
     {
+        public static IEnumerable<string> GetWizardsByRowling(){
+            var repo = Wizard.Wizards.Value;
+
+            var gr = from c in repo
+                     where c.Creator.Contains("Rowling")
+                     select c.Name;
+            
+            return gr;
+        }
+
+        public static IEnumerable<string> GetWizardsByRowlingLinq(){
+            var repo = Wizard.Wizards.Value;
+
+            var gr = repo
+                     .Where(c => c.Creator.Contains("Rowling"))
+                     .Select(c => c.Name);
+
+            return gr;
+        }
+
+        public static int? YearOfTheFirstSithLord(){
+            var repo = Wizard.Wizards.Value;
+
+            var gr = from c in repo
+                     where c.Name.Contains("Darth")
+                     orderby c.Year ascending
+                     select c.Year;
+
+            return gr.ElementAt(0);
+        }
+
+        public static int? YearOfTheFirstSithLordLinq(){
+            var repo = Wizard.Wizards.Value;
+
+            var gr = repo
+                     .Where(c => c.Name.Contains("Darth"))
+                     .OrderBy(c => c.Year)
+                     .Select(c => c.Year);
+
+            return gr.ElementAt(0);
+        }
+
+        public static IEnumerable<(string, int?)> HarryPotterWizards(){
+            var repo = Wizard.Wizards.Value;
+
+            var gr = from c in repo
+                     where c.Medium.Contains("Harry Potter")
+                     select (c.Name, c.Year);
+
+            return gr;
+        }
+
+        public static IEnumerable<(string, int?)> HarryPotterWizardsLinq(){
+            var repo = Wizard.Wizards.Value;
+
+            var gr = repo
+                     .Where(c => c.Medium.Contains("Harry Potter"))
+                     .Select(c => (c.Name, c.Year));
+
+            return gr;
+        }
+
+        public static IEnumerable<string> ListWizardsInReverseOrder(){
+            var repo = Wizard.Wizards.Value;
+
+            var gr = from c in repo
+                     orderby c.Creator descending, c.Name descending
+                     select c.Name;
+
+            return gr.ToArray();
+        }
+
+        public static IEnumerable<string> ListWizardsInReverseOrderLinq(){
+            var repo = Wizard.Wizards.Value;
+
+            var gr = repo
+                     .OrderByDescending(c => (c.Creator, c.Name))
+                     .Select(c => c.Name);
+
+            return gr.ToArray();
+        }
     }
 }
