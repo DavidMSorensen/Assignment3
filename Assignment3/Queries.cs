93de049e24e4f89122d1ac02c47af0461584a8ca
daso <daso@itu.dk>
2021-09-27 10:41:11 +0200

    QueriesTest implemented for queries 1-3

--- Assignment3/Queries.cs
+++ Assignment3/Queries.cs
@@ -1,6 +1,20 @@
+using System;
+using System.Collections.Generic;
+
 namespace BDSA2020.Assignment02
 {
     public class Queries
     {
+        public static string[] printRowling(){
+            throw new NotImplementedException();
+        }
+
+        public static int yearOfTheFirstSithLord(){
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<(string, int)> HarryPotterWizards(){
+            throw new NotImplementedException();
+        }
     }
 }
