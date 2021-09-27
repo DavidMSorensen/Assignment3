a6c0f3830604a4a7e415999694750d6a2729ba7a
ches <ches@itu.dk>
2021-09-27 10:43:25 +0200

    Queries

--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -14,7 +14,15 @@ namespace BDSA2020.Assignment02
         public static IEnumerable<int> FilterLeapYears(this int[] ys) => ys.Where(i => DateTime.IsLeapYear(i));
 
         public static bool IsSecure(this Uri u) => u.ToString().Contains("https");
-        
         public static int WordCount(this string s) => Regex.Split(s, @"\P{L}+").Length;
+
+        //Used for testing Queries
+        public static void Print<T>(this IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
 }
--- Assignment3/Queries.cs
+++ Assignment3/Queries.cs
@@ -1,6 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
 namespace BDSA2020.Assignment02
 {
     public class Queries
     {
+        public static IEnumerable<string> PrintRowling(){
+            var repo = Wizard.Wizards.Value;
+
+            var gr = from c in repo
+                     where c.Creator.Contains("Rowling")
+                     select c.Name;
+            
+            return gr;
+        }
+
+        public static IEnumerable<string> PrintRowlingLinq(){
+            var repo = Wizard.Wizards.Value;
+
+            var gr = repo
+                     .Where(c => c.Creator.Contains("Rowling"))
+                     .Select(c => c.Name);
+
+            return gr;
+        }
+
     }
 }
